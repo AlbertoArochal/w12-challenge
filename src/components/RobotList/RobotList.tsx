0a242ef6720fde39276233bd1de6ba8a030f51d8
AlbertoArochal <arochaldev@gmail.com>
2022-12-31 17:40:02 +0100

    fixes on robot list

--- src/components/RobotList/RobotList.tsx
+++ src/components/RobotList/RobotList.tsx
@@ -1,5 +1,7 @@
 import { RobotGenerator } from '../Robot/Robot';
 import { deities } from './StaffModel';
+import { useEffect } from 'react';
+import { roboScroll } from '../../helpers/roboScroll';
 
 export interface robotType {
     name: string;
@@ -12,6 +14,12 @@ export interface RobotListProps {
 }
 
 export const RobotList = (props: RobotListProps) => {
+    useEffect(() => {
+        if (document.querySelector('.robolist')) {
+            roboScroll();
+        }
+    }, []);
+
     return (
         <div className="robolist">
             {props.robots.map((deity, index: number) => (
---
+++ src/helpers/roboScroll.tsx
@@ -0,0 +1,27 @@
+export const roboScroll = () => {
+    const roboList: HTMLElement | null = document.querySelector('.robolist');
+
+    let isDragging = false;
+    let currentX;
+    let initialX: number;
+    let xOffset = 0;
+
+    roboList!.addEventListener('mousedown', (e: any) => {
+        initialX = e.clientX - xOffset;
+        isDragging = true;
+    });
+
+    roboList!.addEventListener('mouseup', () => {
+        isDragging = false;
+    });
+
+    roboList!.addEventListener('mousemove', (e: any) => {
+        if (isDragging) {
+            e.preventDefault();
+            currentX = e.clientX - initialX;
+            xOffset = currentX;
+
+            roboList!.style.transform = `translateX(${currentX}px)`;
+        }
+    });
+};

eddfbb0dacecfd9b3e3d7294dddaaea42618f4b5
AlbertoArochal <arochaldev@gmail.com>
2023-01-01 22:07:09 +0100

    added roboinfo page

---
+++ src/helpers/onSubmit.ts
@@ -0,0 +1,23 @@
+import { getRobotId } from './getRoobotID';
+import { robotType } from '../components/RobotList/RobotList';
+import { useHire } from '../components/hooks/useHire';
+export const onSubmit = (data: any) => {
+    console.log(data);
+    const createValues = (name: string) => {
+        const velocity = (name.charCodeAt(0) % 10) + 1;
+        const endurance =
+            ((name.charCodeAt(1) + name.charCodeAt(name.length - 1)) % 10) + 1;
+        return [velocity, endurance];
+    };
+    const stats = createValues(data.name);
+    const robot = {
+        name: data.name,
+        id: 0,
+        velocity: stats[0],
+        endurance: stats[1],
+        created_at: new Date().toISOString().slice(0, 10),
+        manufacturer: data.owner,
+    };
+    robot.id = getRobotId(robot);
+    localStorage.setItem('robot', JSON.stringify(robot));
+};

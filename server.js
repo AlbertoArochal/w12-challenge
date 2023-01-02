ca886b534b320f4f06ae5383341a873c7b5f8817
AlbertoArochal <arochaldev@gmail.com>
2023-01-02 20:12:25 +0100

    readed vercel.json

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-
+const cors = require('cors');
+const port = process.env.PORT || 5000;
 server.use(middlewares);
+server.use(jsonServer.rewriter({ '/api/*': '/$1' }));
+
 server.use(router);
-server.listen(3000, () => {
-    console.log('JSON Server is running');
+server.use(cors);
+server.listen(port, () => {
+    console.log(`Server is running on ${port}`);
 });

61b5515764183f4af72a69e8f055885faf3b4790
AlbertoArochal <arochaldev@gmail.com>
2023-01-08 04:02:46 +0100

    src deleted

--- src/components/BestEmployee/EmployeeTyoe.ts
+++
@@ -1,9 +0,0 @@
-export interface EmployeeType {
-    name: string;
-    velocity: number;
-    endurance: number;
-    created_at: string;
-    manufacturer: string;
-    id: string;
-    hired: boolean;
-}
--- src/helpers/getRoobotID.ts
+++
@@ -1,9 +0,0 @@
-import { RobotType } from '../components/RobotList/Catalogue';
-
-export const getRobotId = (robot: RobotType) => {
-    let id = 0;
-    for (let i = 0; i < robot.name.length; i++) {
-        id += robot.name.charCodeAt(i);
-    }
-    return id;
-};

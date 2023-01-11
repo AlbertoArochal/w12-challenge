1baa29669bfc074e7c8ea2b7e9a72a368b8545de
AlbertoArochal <arochaldev@gmail.com>
2023-01-11 22:00:40 +0100

    added more tests for details button

--- src/components/Buttons/Details.test.tsx
+++ src/components/Buttons/Details.test.tsx
@@ -5,6 +5,37 @@ import { MemoryRouter, Route } from 'react-router-dom';
 
 const mockedUsedNavigate = jest.fn();
 
+const localStorageMock = (function () {
+    let store: any = {
+        robot: '{"name":"test","owner":"test","id":1,"upgrades":[]}',
+        targetRobot: '{"name":"test","owner":"test","id":1,"upgrades":[]}',
+    };
+
+    return {
+        getItem(key: any) {
+            return store[key];
+        },
+
+        setItem(key: any, value: any) {
+            store[key] = value;
+        },
+
+        clear() {
+            store = {};
+        },
+
+        removeItem(key: any) {
+            delete store[key];
+        },
+
+        getAll() {
+            return store;
+        },
+    };
+})();
+
+Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+
 jest.mock('../Robot/Robot', () => {
     return {
         RobotGenerator: jest.fn().mockImplementation(() => {
@@ -56,4 +87,42 @@ test('should render a button', async () => {
     );
     const button = screen.getByText('Details');
     expect(button).toBeInTheDocument();
+});
+
+test('should redirect to /hired', async () => {
+    render(
+        <MemoryRouter>
+            <DetailsButton name="Robot 1" />
+        </MemoryRouter>
+    );
+    fireEvent.click(screen.getByText('Details'));
+    expect(mockedUsedNavigate).toBeCalledTimes(0);
+});
+
+const robot = {
+    name: 'Robot1',
+    velocity: 10,
+    endurance: 10,
+    manufacturer: 'Company1',
+    id: 1,
+};
+
+describe('DetailsButton component', () => {
+    beforeEach(() => {
+        localStorage.clear();
+        JSON.stringify(localStorage.setItem('RobotDetails', robot)!);
+    });
+
+    test('should save robot details to localStorage', async () => {
+        const { getByText } = render(<DetailsButton name={robot.name} />);
+
+        global.fetch = jest.fn().mockResolvedValue({
+            json: () => Promise.resolve([robot]),
+        });
+
+        fireEvent.click(getByText('Details'));
+
+        const savedRobot = JSON.parse(JSON.stringify(robot));
+        expect(savedRobot).toEqual(robot);
+    });
 });
